46ada8403c5cc5767a2df435b4b296aee06b3a89
John Barry <JohnBar216@gmail.com>
2018-01-20 16:28:13 -0600

    Initial commit. Set up config loading and initializing the discord bot.

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Snakeyesz/snek-bot/utils"
+)
+
+// Bot Entry Point
+func main() {
+
+	// init discord
+	discord := utils.GetDiscordSession()
+
+	// connect discord bot
+	err := discord.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// Run bot until connection is closed or interupted
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	botRuntimeCh := make(chan os.Signal, 1)
+	signal.Notify(botRuntimeCh, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	// close bot when signal to close is recieved in the botRuntime channel
+	<-botRuntimeCh
+	fmt.Println("Bot is now closeing.")
+	discord.Close()
+}
