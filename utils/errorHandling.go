63814c31d2e7c02092a727262b781a8f784edd94
John Barry <JohnBar216@gmail.com>
2018-01-29 22:56:56 -0600

    Adds function to alert user of error based on i18n file

--- utils/errorHandling.go
+++ utils/errorHandling.go
@@ -1,11 +1,29 @@
 package utils
 
-// This is meant to simplify error checking throughout the application.
-// It may also be expanded upon later for more specific error checking or handling and better logging of errors
+import (
+	"github.com/bwmarrin/discordgo"
+
+	"github.com/Snakeyesz/snek-bot/cache"
+)
 
 // Will panic if error is not nil
 func PanicCheck(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// alert user of custom error if any exists
+func AlertUserOfError(err error, session *discordgo.Session, msg *discordgo.Message) {
+	translations := cache.Geti18nTranslations()
+	errCode := err.Error()
+
+	// check if the error code has a user translation
+	if !translations.ExistsP(errCode) {
+		return
+	}
+
+	// output translation to user
+	translation := translations.Path(errCode).Data().(string)
+	session.ChannelMessageSend(msg.ChannelID, translation)
 }
