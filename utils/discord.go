e6f1dc1242fcb67466c9f09964b0182b57ef645e
John Barry <JohnBar216@gmail.com>
2018-02-04 19:22:53 -0600

    Identified an issue with the discordgo library in regards from moving the bot from one voice channel to another. Fixed this with a work around.

--- utils/discord.go
+++ utils/discord.go
@@ -2,12 +2,15 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/Snakeyesz/snek-bot/cache"
 	"github.com/bwmarrin/discordgo"
 )
 
 // JoinUserVoiceChat bot joins the users voice chat if it can find one, returns an error if it can not
-func JoinUserVoiceChat(session *discordgo.Session, msg *discordgo.Message) (*discordgo.VoiceConnection, error) {
+func JoinUserVoiceChat(msg *discordgo.Message) (*discordgo.VoiceConnection, error) {
+	session := cache.GetDiscordSession()
 
 	// Find the guild based on the channel the message come from
 	channel, err := session.State.Channel(msg.ChannelID)
@@ -17,14 +20,42 @@ func JoinUserVoiceChat(session *discordgo.Session, msg *discordgo.Message) (*dis
 	}
 
 	// Look for the message sender in that guild's current voice states.
-	for _, vs := range guild.VoiceStates {
-		if vs.UserID == msg.Author.ID {
+	for _, voiceState := range guild.VoiceStates {
+		if voiceState.UserID == msg.Author.ID {
+
+			// check if we already have a voice connection in the given guild
+			if voiceConnection, ok := session.VoiceConnections[channel.GuildID]; ok {
+
+				// if connection is in the same channel, no more needs to be done. return the voice connection
+				if voiceConnection.ChannelID == voiceState.ChannelID {
+					return voiceConnection, nil
+				}
+
+				// reaching this means we're changing from one channel to another in the same guild
+				//   must disconnect before reconnecting to new channel
+				voiceConnection.Disconnect()
+			}
 
 			// join the voice channel and return a possible error
-			vc, err := session.ChannelVoiceJoin(guild.ID, vs.ChannelID, false, true)
-			return vc, err
+			voiceConnection, err := session.ChannelVoiceJoin(guild.ID, voiceState.ChannelID, false, true)
+			return voiceConnection, err
 		}
 	}
 
 	return nil, errors.New("bot.voice.no-target-voice-channel")
+}
+
+// SendMessage alerts user of custom error if any exists
+func SendMessage(channelId string, message string) {
+	translations := cache.Geti18nTranslations()
+
+	fmt.Printf("Sending Message...: %s \n", message)
+
+	// check if the error code has a user translation
+	if translations.ExistsP(message) {
+		message = translations.Path(message).Data().(string)
+	}
+
+	// output translation to user
+	cache.GetDiscordSession().ChannelMessageSend(channelId, message)
 }
