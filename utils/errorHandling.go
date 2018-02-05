3fe5fec05435dcaeb1dd35326e3e314696223db0
John Barry <JohnBar216@gmail.com>
2018-02-04 19:19:42 -0600

    Function no longer needed as messages will be sent a different way

--- utils/errorHandling.go
+++ utils/errorHandling.go
@@ -1,29 +1,8 @@
 package utils
 
-import (
-	"github.com/bwmarrin/discordgo"
-
-	"github.com/Snakeyesz/snek-bot/cache"
-)
-
 // Will panic if error is not nil
 func PanicCheck(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-// alert user of custom error if any exists
-func AlertUserOfError(err error, session *discordgo.Session, msg *discordgo.Message) {
-	translations := cache.Geti18nTranslations()
-	errCode := err.Error()
-
-	// check if the error code has a user translation
-	if !translations.ExistsP(errCode) {
-		return
-	}
-
-	// output translation to user
-	translation := translations.Path(errCode).Data().(string)
-	session.ChannelMessageSend(msg.ChannelID, translation)
 }
